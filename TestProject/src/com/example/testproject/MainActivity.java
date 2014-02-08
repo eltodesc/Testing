830b4a6218bcb9e41570f915c74e14e2f2ceec5d
Erik Todeschini <eltodesc@gmail.com>
2014-02-07 20:57:44 -0500

    Changed Cheers to Salute!

--- TestProject/src/com/example/testproject/MainActivity.java
+++ TestProject/src/com/example/testproject/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Toast.makeText(this, "Cheers!", Toast.LENGTH_LONG).show();
+		Toast.makeText(this, "Salute!", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
